4fbbdb57189f0f757b76a6eb46c6cf0f750b0c3b
ieeah <lbnfnc90@gmail.com>
2024-11-29 13:10:34 +0100

    feat(Icons): added icons import

--- src/app/components/Icon/Icons.tsx
+++ src/app/components/Icon/Icons.tsx
@@ -1,18 +1,19 @@
 import { IconProps } from "./types";
 
 import CheckIcon from "./icons/CheckIcon";
-import CloseIcon from "./icons/CloseIcon";
+import ChevronIcon from "./icons/ChevronIcon";
 import ClockIcon from "./icons/ClockIcon";
-import { CloudOnIcon, CloudOffIcon } from "./icons/CloudIcons";
+import CloseIcon from "./icons/CloseIcon";
+import { CloudOffIcon, CloudOnIcon } from "./icons/CloudIcons";
 import { DarkOffIcon, DarkOnIcon } from "./icons/DarkIcons";
 import DeleteIcon from "./icons/DeleteIcon";
 import EditIcon from "./icons/EditIcon";
 import { EyeOffIcon, EyeOnIcon } from "./icons/EyeIcons";
 import InfoIcon from "./icons/InfoIcon";
 import KebabIcon from "./icons/KebabIcon";
+import { LightOffIcon, LightOnIcon } from "./icons/LightIcons";
 import LightningIcon from "./icons/LightningIcon";
 import { LockOffIcon, LockOnIcon } from "./icons/LockIcons";
-import { LightOffIcon, LightOnIcon } from "./icons/LightIcons";
 import {
   NotificationOffIcon,
   NotificationOnIcon,
@@ -21,32 +22,31 @@ import RefreshIcon from "./icons/RefreshIcon";
 import SettingsIcon from "./icons/SettingsIcon";
 import TogglesIcon from "./icons/TogglesIcon";
 import { UserOffIcon, UserOnIcon } from "./icons/UserIcons";
-import ChevronIcon from "./icons/ChevronIcon";
 
 const iconMap = {
-  check: CheckIcon,
-  close: CloseIcon,
-  clock: ClockIcon,
+  "check": CheckIcon,
+  "close": CloseIcon,
+  "clock": ClockIcon,
   "cloud-off": CloudOffIcon,
   "cloud-on": CloudOnIcon,
   "dark-off": DarkOffIcon,
   "dark-on": DarkOnIcon,
-  delete: DeleteIcon,
-  edit: EditIcon,
+  "delete": DeleteIcon,
+  "edit": EditIcon,
   "eye-off": EyeOffIcon,
   "eye-on": EyeOnIcon,
-  info: InfoIcon,
-  kebab: KebabIcon,
-  lightning: LightningIcon,
+  "info": InfoIcon,
+  "kebab": KebabIcon,
+  "lightning": LightningIcon,
   "lock-off": LockOffIcon,
   "lock-on": LockOnIcon,
   "light-off": LightOffIcon,
   "light-on": LightOnIcon,
   "notification-off": NotificationOffIcon,
   "notification-on": NotificationOnIcon,
-  refresh: RefreshIcon,
-  settings: SettingsIcon,
-  toggles: TogglesIcon,
+  "refresh": RefreshIcon,
+  "settings": SettingsIcon,
+  "toggles": TogglesIcon,
   "user-off": UserOffIcon,
   "user-on": UserOnIcon,
   "chevron-left": ChevronIcon,
