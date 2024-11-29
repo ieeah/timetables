aee3eb31edd7d01bbdac21068053f05f8af26216
ieeah <lbnfnc90@gmail.com>
2024-11-29 15:50:53 +0100

    Revert "refactor: moved Password Input component to it's folder"
    This reverts commit b6916d608a318db17e53b2d4c21308a8be0c4eff.

---
+++ src/app/components/Input/Password/Password.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import React from "react";
+import { Icon } from "../../Icon/Icon";
+import { BasicInput } from "../Input";
+import { InputProps } from "../types";
+
+export default function Password(props: InputProps) {
+  const [showPassword, setShowPassword] = React.useState(false);
+  const overrideType = showPassword ? "text" : "password";
+
+  return (
+    <div
+      className="password-input-wrapper"
+      style={{
+        position: "relative",
+        width: "100%"
+      }}
+    >
+      <BasicInput {...props} type={overrideType} />
+      <button
+        style={{
+          position: "absolute",
+          right: "10px",
+          top: "50%",
+          transform: "translateY(-50%)",
+          background: "transparent",
+          border: "none",
+          cursor: "pointer",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          padding: 0,
+        }}
+        onClick={() => setShowPassword(!showPassword)}
+        type="button"
+        aria-label={showPassword ? "Nascondi password" : "Mostra password"}
+        aria-pressed={showPassword}
+      >
+        <Icon icon={showPassword ? "eye-off" : "eye-on"} size="sm" />
+      </button>
+    </div>
+  );
+}
