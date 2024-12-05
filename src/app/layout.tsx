c4582d80dd2de7a7351b6ee821a7e60bbc49df1a
ieeah <lbnfnc90@gmail.com>
2024-12-05 22:47:04 +0100

    feat: add homepage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
-import type { Metadata } from "next";
+"use server";
+
 import { Roboto } from "../../fonts/fonts";
 import Footer from "./components/Footer/Footer";
 import styles from "./home.module.css";
@@ -6,20 +7,22 @@ import "./style/globals/globals.css";
 import "./style/globals/reset.css";
 import "./style/globals/utilities.css";
 
-export const metadata: Metadata = {
-  title: "Timetables",
-  description: "Il tool per capire a che ora puoi uscire venerdì dall' ufficio",
-};
-
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="it">
+      <head>
+        <title>Timetables</title>
+        <meta
+          name="description"
+          content="Il tool per capire a che ora puoi uscire venerdì dall' ufficio"
+        />
+      </head>
       <body className={`${Roboto.className} ${styles.layout}`}>
-        <div className={styles.pageWrapper}>{children}</div>
+        {children}
         <Footer />
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
+import Link from "next/link";
+
+import "./components/Button/button.scss";
+import PageTitle from "./components/PageTitle/PageTitle";
+import "./style/home.css";
+
 export default function Home() {
   return (
-    <main>
-      <h1 className="heading">Timetables</h1>
+    <main className="home">
+      <div className="gradient-wrapper">
+        <PageTitle subtitle={"Il tool per capire a che ora puoi uscire venerdì dall' ufficio"} />
+      </div>
+      <div className="login-btn-wrapper">
+        <Link href="/login" className="btn primary login-btn">Accedi</Link>
+      </div>
+      <div className="gradient-wrapper"></div>
     </main>
   );
 }
